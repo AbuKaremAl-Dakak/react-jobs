715e5c030e27069dad3e27fa726224a02164fe0e
abukarem <abukarem11311@gmail.com>
2024-07-30 11:46:05 +0300

    finish project

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -10,9 +10,7 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchingDate = async () => {
-      const apiUrl = isHome
-        ? "http://localhost:8000/jobs?_limit=3"
-        : "http://localhost:8000/jobs";
+      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
       try {
         const res = await fetch(apiUrl);
 
@@ -33,17 +31,15 @@ const JobListings = ({ isHome = false }) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {loading ? (
-            <Spinner loading={loading} />
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {jobs.map((job) => (
-                <JobListing key={job.id} job={job} />
-              ))}
-            </div>
-          )}
-        </div>
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
